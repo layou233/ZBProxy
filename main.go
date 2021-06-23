99cb0bb3b8d5474c810c4727c3e79b2714eb666f
layou233 <1665571328@qq.com>
2021-06-23 11:55:29 +0800

    Finished first version

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"time"
 )
 
-var onlinePlayers = 0
+var onlineConnections = 0
 
 const (
 	version = "1.0-SNAPSHOT"
 
-	serverAddr = "mc.remiaft.com"
-	serverPort = 25565
-	localPort  = 25565
+	serverAddr        = "mc.hypixel.net"
+	serverPort uint16 = 25565 // this must be uint16 (unsigned short) to be compatible with the protocol
+	localPort  uint16 = 25565
 )
 
 func main() {
@@ -31,7 +31,7 @@ func main() {
 	listen, err1 := net.Listen("tcp", fmt.Sprintf(":%d", localPort))
 	if err1 != nil {
 		log.Printf("Unable to listen on port %d.\n", localPort)
-		log.Printf("Caution: %d\n", err1.Error())
+		log.Printf("Caution: %v\n", err1.Error())
 		log.Printf("The program will exit in 5 seconds.")
 		time.Sleep(5 * time.Second)
 		os.Exit(1)
@@ -40,7 +40,7 @@ func main() {
 	for {
 		time.Sleep(time.Second)
 		windows.SetTitle(
-			fmt.Sprintf("ZBProxy %v | Online Connections: %v", version, onlinePlayers/2))
+			fmt.Sprintf("ZBProxy %v | Online Connections: %v", version, onlineConnections/2))
 		fromConn, err2 := listen.Accept()
 		if err2 != nil {
 			continue
--- transfer.go
+++ transfer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net"
 )
@@ -8,32 +9,32 @@ import (
 func forDial(fromConn net.Conn, forAddr string) {
 	toConn, err := net.Dial("tcp", forAddr)
 	if err != nil {
-		log.Printf("[Closed] %s to %s", fromConn.LocalAddr().String(), toConn.RemoteAddr().String())
+		log.Printf("[Bad Connection] %s to %s", fromConn.LocalAddr().String(), toConn.RemoteAddr().String())
 		toConn.Close()
 		return
 	}
 	log.Printf("[Transfer started] %s to %s", fromConn.LocalAddr().String(), toConn.RemoteAddr().String())
-	go transfer(fromConn, toConn, 1024)
-	go transfer(toConn, fromConn, 1024)
+	go transfer(fromConn, toConn, 1024, true)
+	go transfer(toConn, fromConn, 1024, false)
 }
 
+/*
 func toDial(fromConn net.Conn) {
-	toAddr := fromConn.RemoteAddr()
-	toConn, err := net.Dial("tcp", toAddr.String())
+	toConn, err := net.Dial("tcp", fromConn.RemoteAddr().String())
 	if err != nil {
 		log.Printf("[Closed] %s to %s", fromConn.LocalAddr().String(), toConn.RemoteAddr().String())
 		toConn.Close()
 		return
 	}
 	log.Printf("%s to %s", fromConn.LocalAddr().String(), toConn.RemoteAddr().String())
-	go transfer(fromConn, toConn, 1024)
-	go transfer(toConn, fromConn, 1024)
-}
+	go transfer(fromConn, toConn, 1024, true)
+	go transfer(toConn, fromConn, 1024, false)
+}*/
 
-func transfer(f, t net.Conn, n int) {
+func transfer(f, t net.Conn, n int, isFrom2to bool) {
 	firstConn, secondConn := true, false
-	onlinePlayers++
-	defer func() { onlinePlayers-- }()
+	onlineConnections++
+	defer func() { onlineConnections-- }()
 	defer f.Close()
 	defer t.Close()
 
@@ -44,18 +45,31 @@ func transfer(f, t net.Conn, n int) {
 			break
 		}
 		if firstConn {
-			log.Println(buf)
+			if isFrom2to && buf[1] == 0 {
+				addressLength := DecodeVarint(buf, 3)
+				//log.Println(addressLength)
+				buf = bytes.Join([][]byte{
+					buf[:3],
+					{(byte)(len(serverAddr))},
+					[]byte(serverAddr),
+					{byte(serverPort >> 8), byte(serverPort & 0xff)}, // uint16 to []byte aka []uint8
+					buf[3+addressLength+2+1:],                        // 2 is the length of 2* unsigned short (uint16)
+				}, make([]byte, 0))
+				buf[0] = (byte)((int)(buf[0]) + len(serverAddr) - addressLength)
+				count += len(serverAddr) - addressLength
+			}
+			//log.Println(buf)
 			firstConn = false
 			secondConn = true
 		} else if secondConn {
 			log.Printf("[ATTENTION] A new player has joined the game.")
-			log.Println(buf)
+			//log.Println(buf)
 			defer func() { log.Printf("[Closed] %s to %s", f.RemoteAddr().String(), t.RemoteAddr().String()) }()
 			secondConn = false
 		}
 		count, err = t.Write(buf[:count])
 		if err != nil {
-			log.Printf("fault,err: %s", err.Error())
+			log.Printf("err: %s", err.Error())
 			break
 		}
 	}
