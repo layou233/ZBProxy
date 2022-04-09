9bb37b13dd551751275968d3c5e05dd7804cfda2
B站贴吧蜡油 <1665571328@qq.com>
2022-04-09 15:21:43 +0800

    no zerocopy supported for Windows

--- service/transfer/plain.go
+++ service/transfer/plain.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"runtime"
 )
 
 const (
@@ -27,17 +28,22 @@ func SimpleTransfer(a, b *net.TCPConn, flow int) {
 		go io.Copy(writerOnly{b}, a)
 		io.Copy(writerOnly{a}, b)
 
-	case FLOW_LINUX_ZEROCOPY:
-		log.Print(color.HiRedString("The `linux-zerocopy` is deprecated, please use `zerocopy` instead."))
-		fallthrough
-
 	case FLOW_ZEROCOPY:
 		fallthrough
 
+	case FLOW_LINUX_ZEROCOPY:
+		if runtime.GOOS != "linux" {
+			log.Panic(color.HiRedString("Only Linux based systems support Linux ZeroCopy, please set your flow to origin or auto."))
+		}
+		fallthrough
+
 	case FLOW_AUTO:
-		go zerocopy.CopyTCP(b, a)
-		zerocopy.CopyTCP(a, b)
-		return // TODO: Use MULTIPLE when fail to sendfile or splice
+		if runtime.GOOS == "linux" {
+			go zerocopy.CopyTCP(b, a)
+			zerocopy.CopyTCP(a, b)
+			return // TODO: Use MULTIPLE when fail to sendfile or splice
+		}
+		fallthrough
 
 	case FLOW_MULTIPLE:
 		aReader := buf.NewReader(a)
