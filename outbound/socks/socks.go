2805549a226544e6da0ab2bd20e1ef536cc53bd2
B站贴吧蜡油 <admin@launium.com>
2022-07-21 21:56:20 +0800

    switch to openssh SOCKS document archive

--- outbound/socks/socks.go
+++ outbound/socks/socks.go
@@ -1,8 +1,8 @@
 // Package socks implemented SOCKS protocol version 4, 4a and 5 (RFC1928).
 //
-// http://ftp.icm.edu.pl/packages/socks/socks4/SOCKS4.protocol
+// http://www.openssh.com/txt/socks4.protocol
 //
-// http://www.smartftp.com/Products/SmartFTP/RFC/socks4a.protocol
+// http://www.openssh.com/txt/socks4a.protocol
 //
 // http://www.rfc-editor.org/rfc/rfc1928.txt
 package socks
