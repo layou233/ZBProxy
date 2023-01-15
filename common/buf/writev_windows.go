1b7e58b40d583598ae87400923741c056d7752ce
layou233 <admin@launium.com>
2023-01-15 15:41:39 +0800

    adjust: Improve Windows WriteV

--- common/buf/writev_windows.go
+++ common/buf/writev_windows.go
@@ -7,18 +7,24 @@ import (
 	"syscall"
 )
 
-type windowsWriter struct{}
+type windowsWriter struct {
+	bufs []syscall.WSABuf
+}
 
-func (w windowsWriter) Write(fd uintptr, buffers net.Buffers) int64 {
-	bufs := make([]syscall.WSABuf, 0, len(buffers))
+func (w *windowsWriter) Write(fd uintptr, buffers net.Buffers) int64 {
+	if w.bufs == nil {
+		w.bufs = make([]syscall.WSABuf, 0, len(buffers))
+	} else {
+		w.bufs = w.bufs[:0]
+	}
 	for _, buffer := range buffers {
-		bufs = append(bufs, syscall.WSABuf{
+		w.bufs = append(w.bufs, syscall.WSABuf{
 			Len: uint32(len(buffer)),
 			Buf: &buffer[0],
 		})
 	}
 	var n uint32
-	if syscall.WSASend(syscall.Handle(fd), &bufs[0], uint32(len(bufs)), &n, 0, nil, nil) == nil {
+	if syscall.WSASend(syscall.Handle(fd), &w.bufs[0], uint32(len(w.bufs)), &n, 0, nil, nil) == nil {
 		return int64(n)
 	}
 	return -1
