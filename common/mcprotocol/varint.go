37984dcbe15db3daf37ca1bab169d30d2563b792
layou233 <admin@launium.com>
2023-01-26 09:07:01 +0800

    adjust: Improve VarInt write

--- common/mcprotocol/varint.go
+++ common/mcprotocol/varint.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io"
 
+	"github.com/layou233/ZBProxy/common/buf"
 	"github.com/layou233/ZBProxy/common/rw"
 )
 
@@ -42,6 +43,49 @@ func (i VarInt) WriteTo(w io.Writer) (n int64, err error) {
 	return int64(nn), err
 }
 
+func (i VarInt) WriteToBuffer(buffer *buf.Buffer) {
+	i32 := int32(i)
+	WriteVarIntTo(buffer.Extend(VarIntLen(i32)), i32)
+}
+
+// WriteVarIntTo writes an encoded VarInt into bs.
+// Note that it would panic when bs length not enough.
+func WriteVarIntTo(bs []byte, n int32) (i int) {
+	num := uint32(n)
+	numWrite := 0
+	for {
+		b := num & 0x7F
+		num >>= 7
+		if num != 0 {
+			b |= 0x80
+		}
+		bs[numWrite] = byte(b)
+		numWrite++
+		if num == 0 {
+			break
+		}
+	}
+
+	return numWrite
+}
+
+func VarIntLen(n int32) int {
+	switch {
+	case n < 0:
+		return 5
+	case n < 128:
+		return 1
+	case n < 16384:
+		return 2
+	case n < 2097152:
+		return 3
+	case n < 268435456:
+		return 4
+	default:
+		return 5
+	}
+}
+
 func ReadVarIntFrom(r io.Reader) (i int32, n int64, err error) {
 	var V uint32
 	var num int64
@@ -61,24 +105,4 @@ func ReadVarIntFrom(r io.Reader) (i int32, n int64, err error) {
 
 	i = int32(V)
 	return
-}
-
-func EncodeVarInt(n int32) ([MaxVarIntLen]byte, int) {
-	var vi [MaxVarIntLen]byte
-	num := uint32(n)
-	numWrite := 0
-	for {
-		b := num & 0x7F
-		num >>= 7
-		if num != 0 {
-			b |= 0x80
-		}
-		vi[numWrite] = byte(b)
-		numWrite++
-		if num == 0 {
-			break
-		}
-	}
-
-	return vi, numWrite
 }
