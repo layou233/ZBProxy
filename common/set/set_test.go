ee65b5ab10aa0998e83ff32a91e3bf529eb35ff5
layou233 <admin@launium.com>
2023-01-28 09:55:19 +0800

    chore: Add benchmark for StringSet

---
+++ common/set/set_test.go
@@ -0,0 +1,47 @@
+package set
+
+import (
+	"crypto/rand"
+	mrand "math/rand"
+	"testing"
+	//"golang.org/x/exp/slices"
+)
+
+func BenchmarkStringSet_Has(b *testing.B) {
+	const ElementNum = 64
+	listSlice := make([]string, 0, ElementNum)
+	for i := 0; i < ElementNum; i++ {
+		var nameBytes [16]byte
+		rand.Read(nameBytes[:])
+		listSlice = append(listSlice, string(nameBytes[:]))
+	}
+	s := NewStringSetFromSlice(listSlice)
+	target := listSlice[mrand.Int31n(ElementNum)]
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if !s.Has(target) {
+			b.Fatal("target element not found")
+		}
+	}
+}
+
+/*
+func BenchmarkStrings_Contain(b *testing.B) {
+	const ElementNum = 64
+	listSlice := make([]string, 0, ElementNum)
+	for i := 0; i < ElementNum; i++ {
+		var nameBytes [16]byte
+		rand.Read(nameBytes[:])
+		listSlice = append(listSlice, string(nameBytes[:]))
+	}
+	target := listSlice[mrand.Int31n(ElementNum)]
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if !slices.Contains(listSlice, target) {
+			b.Fatal("target element not found")
+		}
+	}
+}
+*/
