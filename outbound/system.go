b75c1d079b4a4b992dcbe5932d8ff83e0d782b36
layou233 <admin@launium.com>
2023-04-02 13:48:18 +0800

    fix: Outbound DialTCP does not apply socket options

--- outbound/system.go
+++ outbound/system.go
@@ -9,6 +9,9 @@ import (
 var SystemOutbound Outbound = &systemOutbound{}
 
 func NewSystemOutbound(control DialerControl) Outbound {
+	if control == nil {
+		return SystemOutbound
+	}
 	return &systemOutbound{
 		Dialer: net.Dialer{Control: control},
 	}
@@ -21,7 +24,18 @@ type systemOutbound struct {
 }
 
 func (o *systemOutbound) DialTCP(network string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error) {
-	return net.DialTCP(network, laddr, raddr)
+	if o.Dialer.Control == nil {
+		return net.DialTCP(network, laddr, raddr)
+	}
+
+	conn, err := (&net.Dialer{
+		LocalAddr: laddr,
+		Control:   o.Dialer.Control,
+	}).Dial(network, raddr.String())
+	if err == nil {
+		return conn.(*net.TCPConn), nil
+	}
+	return nil, err
 }
 
 func (o *systemOutbound) Handshake(_ io.Reader, _ io.Writer, _, _ string) error {
