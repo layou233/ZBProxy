746777e909c1682fca52334a46ddae9dbd373c06
layou233 <admin@launium.com>
2023-08-10 22:28:09 +0800

    chore: Deprecate duplicated WriterV

--- common/buf/copy.go
+++ common/buf/copy.go
@@ -2,35 +2,24 @@ package buf
 
 import (
 	"io"
-	"syscall"
+	"net"
 )
 
 func Copy(dst io.Writer, src *ReaderV) (n int64, err error) {
-	var writerV *WriterV
-	useWriterV := false
-	if sysConn, ok := dst.(syscall.Conn); ok {
-		if rawConn, err := sysConn.SyscallConn(); err == nil {
-			writerV, useWriterV = NewWriterV(dst, rawConn)
-		}
-	}
-
 	for {
-		buffers, err := src.ReadVectorized()
+		var buffers net.Buffers
+		buffers, err = src.ReadVectorized()
 		if err != nil {
 			PutMulti(buffers)
-			return n, err
+			return
 		}
 
 		var written int64
-		if useWriterV {
-			written, err = writerV.WriteVectorized(buffers)
-		} else {
-			written, err = buffers.WriteTo(dst)
-		}
+		written, err = buffers.WriteTo(dst)
 
 		PutMulti(buffers)
 		if err != nil {
-			return n, err
+			return
 		}
 		n += written
 	}
--- common/buf/writev_stub.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package buf
-
-func newVectorizedWriter() (vectorizedWriter, bool) {
-	return nil, false
-}
